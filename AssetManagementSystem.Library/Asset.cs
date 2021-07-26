b62a491691b0d99fa4924b5e6e187e11d037c231
Karan Goyal <karangoyalsit@gmail.com>
2021-07-26 21:31:44 +0530

    Add project files.

---
+++ AssetManagementSystem.Library/Asset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AssetManagementSystem.Library
+{
+    public class Asset
+    {
+        public string MachineName { get; set; }
+        public string AssetName { get; set; }
+        public string SeriesNumber { get; set; }
+
+        public Asset()
+        { }
+
+        public Asset(string machineName)
+        {
+            MachineName = machineName;
+        }
+
+        public Asset(string machineName, string assetName)
+        {
+            MachineName = machineName;
+            AssetName = assetName;
+        }
+
+        public Asset(string machineName, string assetName, string seriesNumber)
+        {
+            MachineName = machineName;
+            AssetName = assetName;
+            SeriesNumber = seriesNumber;
+        }
+    }
+}
---
+++ AssetManagementSystem.Library/AssetManagementServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagementSystem.Library
+{
+    public class AssetManagementServices
+    {
+        List<Asset> _listOfAssets;
+
+        public AssetManagementServices()
+        {
+            GetAssetData();
+        }
+
+        public void GetAssetData()
+        {
+            Asset a1 = new Asset("C300", "Cutterhead", "S6");
+            Asset a2 = new Asset("C40", "Cutter head", "S7");
+            Asset a3 = new Asset("C300", "Blade safety cover", "S10");
+            Asset a4 = new Asset("C60", "Blade safety cover", "S11");
+            Asset a5 = new Asset("C300", "Deburring blades", "S6");
+            Asset a6 = new Asset("C60", "Cutter head", "S8");
+            Asset a7 = new Asset("C60", "Clamping fixture", "S2");
+            Asset a8 = new Asset("C40", "Blade safety cover", "S11");
+            Asset a9 = new Asset("C40", "Shutter gripper", "S3");
+        }
+
+
+    }
+}
